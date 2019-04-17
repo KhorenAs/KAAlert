61ba0225481bbdebd6605c5db2ba0e42b6364c31
MNMKhoren <mnmkasatryan@gmail.com>
2019-04-17 12:22:26 +0400

    first commit

---
+++ KAAlert/Alert/Sound/KASoundEffect.swift
@@ -0,0 +1,13 @@
+//
+//  KASoundEffect.swift
+//  KAAlert
+//
+//  Created by MNMKhoren on 4/16/19.
+//  Copyright © 2019 Asatryasn. All rights reserved.
+//
+
+import UIKit
+
+class KASoundEffect: NSObject {
+
+}
