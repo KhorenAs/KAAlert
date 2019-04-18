dc4dbed5f242786cd25d0bb700232fbd536e8d7c
MNMKhoren <mnmkasatryan@gmail.com>
2019-04-18 14:41:58 +0400

    test

--- Test.swift
+++ Test.swift
@@ -11,7 +11,7 @@ class Test: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("")
         // Do any additional setup after loading the view.
     }
     
